2fae2121eee9b3215495ca6b0b03f0793d7cb073
Denis Patel <200230111007@gcsurat.gujgov.edu.in>
2024-06-25 16:40:53 +0530

    update

--- 2. Under SSIP program/Database_Initialization.sql
+++
@@ -1,4 +0,0 @@
-create database licence_userdata
-use licence_userdata
-create table data(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), date_of_birth DATE, mobile_number VARCHAR(15), license_approval_date DATE, license_expiry_date DATE, license_number VARCHAR(20), rfid_card VARCHAR(50), fingerprint BLOB, photo LONGBLOB)
-use licence_userdata
